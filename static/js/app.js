d77b364feccb20e3296cb61cf5315ee5fe8d5079
Mike Sammis <mike.sammis@ihsmarkit.com>
2020-04-01 13:44:25 -0500

    revision

--- static/js/app.js
+++ static/js/app.js
@@ -12,12 +12,12 @@ function init() {
            
     var initialID = 0;
     demos(initialID)
-    graphs(initialID)
+    plots(initialID)
    });
 };
 
 
-// Function to run when user changes ID
+// ID Changes
 function changeID() {
 
 // Store value entered by user    
@@ -30,17 +30,17 @@ d3.json("samples.json").then((importedSamples) => {
    var index = subjectID.findIndex(function(i){
    return i === newID
 });
-/
 
-// Run demos and graphs function with new values
+
+// Run demos and plots function with new values
 demos(index)
-graphs(index)
+plots(index)
 });
 };
 
 
 // Get subject id metadata
-function getIDMetaData (id) {
+function demos (id) {
 //Metadata
 var demographicInfo = d3.select("#sample-metadata");
 demographicInfo.html("");
@@ -53,13 +53,32 @@ demographicInfo.html("");
    });
 };
 
-// Functions to obatain plots    
-function graphs(id) {    
+// Make plots    
+function plots(id) {    
 
   d3.json("samples.json").then((graphsData) => {    
    
        var graphSamples = graphsData.samples[`${id}`]; 
-       console.log("samples", graphSamples);
+       
+   // Bar Chart
+   var barX = graphSamples.sample_values.slice(0, 10).reverse();
+   var barY = graphSamples.otu_ids.slice(0, 10).map(y => `OTU ${y} `).reverse();
+   
+   var barTrace = {
+       x: barX,
+       y: barY,
+       type: "bar",
+       orientation: "h"
+   };
+   
+  
+   var barData = [barTrace];
+   var barLayout = {
+       title: "Top 10 OTUs per Subject",
+       };
+   Plotly.newPlot("bar", barData, barLayout);
+   
+   
 
     // Bubble Chart
     var bubbleX = graphSamples.otu_ids;
@@ -85,28 +104,7 @@ function graphs(id) {
 
 });   
            
-   // Bar Chart
-       var barX = graphSamples.sample_values.slice(0, 10).reverse();
-       var barY = graphSamples.otu_ids.slice(0, 10).map(y => `OTU ${y} `).reverse();
-       
-       var barTrace = {
-           x: barX,
-           y: barY,
-           type: "bar",
-           orientation: "h"
-       };
-       
-       console.log("bar_x", barX);
-       console.log("bar_y", barY);
-       
-       var barData = [barTrace];
-       var barLayout1 = {
-           title: "Top 10 OTUs per Subject",
-           };
-       Plotly.newPlot("bar", barData, barLayout1);
-       
-       
-   
+
        
        //Gauge Plot
        
@@ -114,8 +112,7 @@ function graphs(id) {
            var metabubbleData = bubbleData.metadata[`${id}`];
        
        var frequency = metabubbleData.wfreq
-       console.log("washing frequncy: ", frequency)
-       
+              
        var gaugeData = [
            {
                domain: { x: [0, 1], y: [0, 1] },
@@ -131,7 +128,7 @@ function graphs(id) {
        ];
        
        var gaugeData = [gaugeTrace];
-       var gaugeLayout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+       var gaugeLayout = { width: 550, height: 600, margin: { t: 0, b: 0 } };
        Plotly.newPlot('gauge', gaugeData, gaugeLayout);        
    
    });
